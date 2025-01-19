d15c62a77a8cae01e41785901fbcb2dd4a9d8562
Lakshmi-ch <llakshmi_ch@yahoo.com>
2025-01-19 18:28:53 +0000

    Added validataion for Contact form

--- pages/home_page_bookingform.js
+++ pages/home_page_bookingform.js
@@ -1,13 +1,9 @@
 
- import { selectors } from "../utils/selectors.json";
- const selector = selectors;
- 
- export async function navigateToHomePage(page, expect, bookingformData){
-  
-  await page.goto('https://automationintesting.online/', { waitUntil: 'domcontentloaded' });
-  await expect(page).toHaveTitle("Restful-booker-platform demo");
+import { selectors } from "../utils/selectors.json";
+const selector = selectors;
+
+export async function bookRoomForm(page, expect, bookingformData) {
   await page.getByText("Book this room").first().click();
-  //await page.waitForTimeout(5000);
   await page.locator(selector.bookingformFirstName).type(bookingformData.firstName);
   await page.locator(selector.bookingformLastName).type(bookingformData.lastName);
   await page.locator(selector.bookingformEmail).type(bookingformData.email);
@@ -17,6 +13,20 @@
   expect(text.includes(bookingformData.expectedErrormessage)).toBe(bookingformData.expected);
 
 }
+export async function contactForm(page, expect, contactformData) {
+  
+ await page.locator(selector.contactformFirstName).type(contactformData.firstName);
+ await page.locator(selector.contactformEmail).type(contactformData.email);
+ await page.locator(selector.contactformPhone).type(contactformData.phone);
+ await page.locator(selector.contactformSubject).type(contactformData.subject);
+ await page.locator(selector.contactformMessage).type(contactformData.message);
+ await page.locator(selector.submitButton).click();
+ await expect(async () => {
+  await expect(page.getByText(contactformData.expectedMessage)).toBeVisible({timeout: 500})
+ }).toPass();
+
+}
+
 
 
 
--- tests/home_page.spec.js
+++ tests/home_page.spec.js
@@ -1,14 +1,31 @@
 import { test, expect } from '@playwright/test';
-import { navigateToHomePage } from 'C:/JavaScriptPrograms/PlaywrightDemo/pages/home_page_bookingform'
-import bookingformData from '../data/bookingform.json';
+import { bookRoomForm } from 'C:/JavaScriptPrograms/PlaywrightDemo/pages/home_page_bookingform'
+import { contactForm } from 'C:/JavaScriptPrograms/PlaywrightDemo/pages/home_page_bookingform'
 
-const validData = bookingformData.valid;
-const invalidData = bookingformData.invalid
+import bookingformData from '../data/bookingform.json';
+import contactformData from '../data/contactform.json';
+
+
+const bookingformvalidData = bookingformData.valid;
+const bookingforminvalidData = bookingformData.invalid
+const validContactformData = contactformData.valid;
+const invalidContactformData = contactformData.invalid
 test.describe('Home Page Tests', () => {
-test('Book the room with the valid data', async ({ page }) => {
-  await navigateToHomePage(page, expect, validData);
-});
-test('Book the room with the invalid data', async ({ page }) => {
-  await navigateToHomePage(page, expect, invalidData);
-});
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://automationintesting.online/', { waitUntil: 'domcontentloaded' });
+    await expect(page).toHaveTitle("Restful-booker-platform demo");
+  });
+  test('Book the room with the valid data', async ({ page }) => {
+    await bookRoomForm(page, expect, bookingformvalidData);
+  });
+  test('Book the room with the invalid data', async ({ page }) => {
+    await bookRoomForm(page, expect, bookingforminvalidData);
+  });
+  test('Contact form validation', async ({ page }) => {
+    await contactForm(page, expect, validContactformData);
+  });
+  test('Contact form validation with invalid data', async ({ page }) => {
+    await contactForm(page, expect, invalidContactformData);
+  });
 });
