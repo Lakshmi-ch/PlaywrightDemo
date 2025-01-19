fd2f65ef96cc0d9001fe792c8d1357f1e1ebc02b
Lakshmi-ch <llakshmi_ch@yahoo.com>
2025-01-19 16:07:31 +0000

    Added Selectors and data in the json file

--- pages/home_page_bookingform.js
+++ pages/home_page_bookingform.js
@@ -1,16 +1,20 @@
 
- export async function navigateToHomePage(page, expect){
+ import { selectors } from "../utils/selectors.json";
+ const selector = selectors;
+ 
+ export async function navigateToHomePage(page, expect, bookingformData){
   
   await page.goto('https://automationintesting.online/', { waitUntil: 'domcontentloaded' });
   await expect(page).toHaveTitle("Restful-booker-platform demo");
   await page.getByText("Book this room").first().click();
-  await page.waitForTimeout(5000);
-  await page.locator("//input[@name = 'firstname']").type("Leela");
-  await page.locator("//input[@name = 'lastname']").type("Cho");
-  await page.locator("//input[@name = 'email']").type("leela@test.com");
-  await page.locator("//input[@name = 'phone']").type("123456789109");
-  await page.locator("//button[text()='Book']").click();
-  await page.waitForTimeout(5000);
+  //await page.waitForTimeout(5000);
+  await page.locator(selector.bookingformFirstName).type(bookingformData.firstName);
+  await page.locator(selector.bookingformLastName).type(bookingformData.lastName);
+  await page.locator(selector.bookingformEmail).type(bookingformData.email);
+  await page.locator(selector.bookingformPhone).type(bookingformData.phone);
+  await page.locator(selector.bookButton).click();
+  const text = await page.locator(selector.errorMessage).textContent();
+  expect(text.includes(bookingformData.expectedErrormessage)).toBe(bookingformData.expected);
 
 }
 
--- tests/home_page.spec.js
+++ tests/home_page.spec.js
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { navigateToHomePage } from 'C:/JavaScriptPrograms/PlaywrightDemo/pages/home_page_bookingform'
+import bookingformData from '../data/bookingform.json';
+
+const validData = bookingformData.valid;
+const invalidData = bookingformData.invalid
 test.describe('Home Page Tests', () => {
-test('has title', async ({ page }) => {
-  await navigateToHomePage(page, expect);
+test('Book the room with the valid data', async ({ page }) => {
+  await navigateToHomePage(page, expect, validData);
+});
+test('Book the room with the invalid data', async ({ page }) => {
+  await navigateToHomePage(page, expect, invalidData);
 });
 });
